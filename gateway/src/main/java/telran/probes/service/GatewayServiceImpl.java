b1dc14e9987d37f71cb42eb303d8592143373f89
DimaOstanin <xnadab@gmail.com>
2024-02-21 18:31:03 +0200

    moves security and communicating with account-provider from

--- gateway/src/main/java/telran/probes/service/GatewayServiceImpl.java
+++ gateway/src/main/java/telran/probes/service/GatewayServiceImpl.java
@@ -12,36 +12,36 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
-public class GatewayServiceImpl implements GatewaySevice {
+public class GatewayServiceImpl implements GatewayService {
 	@Value("#{${app.map.hosts.ports}}")
-	Map<String,String> hostsPortsMap;
-	
+Map<String, String> hostsPortsMap;
 	@Override
 	public ResponseEntity<byte[]> proxyRouting(ProxyExchange<byte[]> proxyExchange, HttpServletRequest request,
 			String httpMethod) {
 		String url = getUrl(request);
-	ResponseEntity<byte[]> res =	switch(httpMethod) {
+		ResponseEntity<byte[]> res = switch(httpMethod) {
 		case "GET" -> proxyExchange.uri(url).get();
 		case "PUT" -> proxyExchange.uri(url).put();
 		case "POST" -> proxyExchange.uri(url).post();
 		case "DELETE" -> proxyExchange.uri(url).delete();
-			default -> throw new IllegalArgumentException("Wrong H Method");
+			default -> throw new IllegalArgumentException("Wrong HTTP method");
 		};
 		return res;
+		
 	}
-
 	private String getUrl(HttpServletRequest request) {
-		String uri = request.getRequestURI();// /sensors/emails/
-		log.debug("received URI:{}",uri);
+		String uri = request.getRequestURI(); //  /accounts/admin@tel-ran.co.il
+		log.debug("received URI: {}", uri);
 		String firstURN = uri.split("/+")[1];
-		log.debug("first URN {}",firstURN);
+		log.debug("first URN {}", firstURN);
 		String hostPort = hostsPortsMap.get(firstURN);
-		String res = String.format("%s%s",hostPort,uri);
+		String res = String.format("%s%s", hostPort, uri);
 		log.debug("result URL is {}", res);
 		return res;
 	}
 	@PostConstruct
 	void logMap() {
-		log.debug("hosts-ports map is {}",hostsPortsMap);
+		log.debug("hosts-ports map is {}",hostsPortsMap );
 	}
+
 }
